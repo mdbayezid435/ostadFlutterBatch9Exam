1cc3c3ba3dd03f71c6f672d37207a35fb5c05c03
Mohammad Bayezid <91377467+mdbayezid435@users.noreply.github.com>
2025-03-07 18:44:48 +0300

    Delete lib/const directory

--- lib/const/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class MyColors {
-  static const colorRed = Color.fromRGBO(231, 28, 36, 1);
-  static const colorgreen = const Color.fromRGBO(33, 191, 115, 1);
-  static const colorwhite = const Color.fromRGBO(255, 255, 255, 1);
-  static const colorDarkBlue = const Color.fromRGBO(44, 62, 80, 1);
-}
