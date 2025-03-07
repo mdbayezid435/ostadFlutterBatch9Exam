c43d7aec897cd544ad00c0ae72879c2ff6bef0f0
Mohammad Bayezid <91377467+mdbayezid435@users.noreply.github.com>
2025-03-07 17:23:05 +0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
-import 'package:crud_project_class/Presentation/screenView/gridScreenView.dart';
+import 'package:crudapp/screens/homepage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main (){
+  runApp(CrudApp());
 }
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Crud app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const GridScreenView(),
+      home: homePage(),
     );
   }
 }
