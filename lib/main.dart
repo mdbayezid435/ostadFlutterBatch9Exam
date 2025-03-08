eca5a598350bcc166ecda1527d39d6399cde8220
Mohammad Bayezid <91377467+mdbayezid435@users.noreply.github.com>
2025-03-08 06:47:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 
+import 'package:crudapp/screens/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(CrudApp());
+}
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homePage(),
+    );
+  }
+}
